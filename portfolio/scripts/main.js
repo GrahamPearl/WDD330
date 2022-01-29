273d5f4b60d508e0d482012a0764ea62dc295dc3
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-01-29 17:55:58 +0200

    update of the home scripts - refactoring

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,32 +1,41 @@
 // toggle menu in small view
 const menuItems =
-    '<li><a id="toggleMenu">&equiv;</a></li>' +
-    '<li><a href="../index.html">Home</a></li>' +
-    '<li><a href="task2.html">Lesson 2</a></li>' +
-    '<li><a href="task3.html" class="active">Lesson 3</a></li>' +
-    '<li><a href="task4.html">Lesson 4</a></li>' +
-    '<li><a href="task5.html">Lesson 5</a></li>' +
-    '<li><a href="task6.html">Lesson 6</a></li>"'
+    '<div class="container-fluid">' +
+    ' <a class="navbar-brand" href="#">Navbar</a>' +
+    '  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+    '    <span class="navbar-toggler-icon"></span>' +
+    '  </button>' +
+    '  <div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+    '    <ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+    '      <li class="nav-item">' +
+    '        <a class="nav-link active" aria-current="page" href="#">Home</a>' +
+    '      </li>' +
+    '      <li class="nav-item">' +
+    '        <a class="nav-link" href="#">Link</a>' +
+    '      </li>' +
+    '      <li class="nav-item dropdown">' +
+    '        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tasks' +
+    '        </a>' +
+    '        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">' +
+    '          <li><a class="dropdown-item" href="#">1</a></li>' +
+    '          <li><a class="dropdown-item" href="#">2</a></li>' +
+    '          <li><a class="dropdown-item" href="#">3</a></li>' +
+    '          <li><hr class="dropdown-divider"></li>' +
+    '          <li><a class="dropdown-item" href="#">Team 3</a></li>' +
+    '        </ul>' +
+    '      </li>' +
+    '    </ul>' +
+    '    <form class="d-flex">' +
+    '      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">' +
+    '      <button class="btn btn-outline-success" type="submit">Search</button>' +
+    '    </form>' +
+    '  </div>' +
+    '</div>'
 
-
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-    document.querySelector("#menu").innerHTML = menuItems
-}
+//document.getElementById("#menu").innerHTML = menuItems
 
 
 //<document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 // set current year in footer
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
-
-const generateLinks = () => {
-
-    let itemU = document.createElement('li');
-    let itemA = document.createElement('a');
-    itemA.textContent = element.linkFor;
-    itemA.setAttribute('href', element.linkURL);
-    itemU.appendChild(itemA);
-
-    document.querySelector('#menu').appendChild(itemU);
-}
