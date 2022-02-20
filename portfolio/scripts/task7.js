8105b3c92b3b3a3ad682ee71651f7ec347a4a9a0
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-02-20 07:19:45 +0200

    Update

--- portfolio/scripts/task7.js
+++ portfolio/scripts/task7.js
@@ -19,3 +19,36 @@ textButton.addEventListener('click', () => {
         .then(text => outputDiv.innerText = text)
         .catch(error => console.log('There was an error:', error))
 }, false);
+
+const form = document.forms['todo'];
+form.addEventListener('submit', addTask, false);
+
+// Used to simulate the adding of a task to a database for a To Do List.
+
+function addTask(event) {
+    event.preventDefault();
+    const number = form.task.value;
+    const task = {
+        userId: 1,
+        title: form.task.value,
+        completed: false
+    }
+    const data = JSON.stringify(task);
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+
+    const headers = new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    });
+    const request = new Request(url, {
+        method: 'POST',
+        header: headers,
+        body: data
+    })
+
+    fetch(request)
+        .then(response => response.json())
+        .then(task => console.log(`Task saved with an id of ${task.id}`))
+        .catch(error => console.log('There was an error:', error))
+
+}
