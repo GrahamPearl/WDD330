bc1a20a84055907a55ee1dde8325cf9b47b4f170
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2022-02-26 16:17:22 +0200

    css and js for demonstration of transforms

--- portfolio/scripts/task8.js
+++ portfolio/scripts/task8.js
@@ -1,1 +1,29 @@
 /* Lesson 8 */
+stylesToAdd = [
+    "transform: scale(1.15, 1.15);",
+    "transform: scale(0.85, 0.85);",
+    "transform: translateX(50px);",
+    "transform: translateX(-50px);",
+    "transform: rotateZ(30deg);",
+    "transform: rotateY(360deg);",
+    "transform: skew(30deg);",
+    "transform-origin: 35px bottom; transform: rotate(90deg);",
+    "transform-origin:  5px left; transform: rotate(90deg);"
+];
+
+let container = document.getElementById("demo");
+
+for (i = 1; i <= 8; i++) {
+    let item = document.createElement("div");
+    let text = document.createTextNode("Hover Over");
+    item.appendChild(text);
+    item.classList.add("card");
+
+    item.onmouseover = function() {
+        this.style.cssText = 'color: #FFFFFF;';
+    };
+
+    item.style.cssText = stylesToAdd[i];
+
+    container.appendChild(item);
+}
